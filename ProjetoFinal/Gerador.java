bf3e284e760780744fd027b6963b4e7cf313878d
Carlos Pereira <56885446+chenrique13@users.noreply.github.com>
2019-11-26 21:11:36 -0300

    Update Gerador.java

--- ProjetoFinal/Gerador.java
+++ ProjetoFinal/Gerador.java
@@ -5,7 +5,11 @@ public class Gerador {
 	private static int senhaPreferencial = 0;
 
 	public static int senhaNormal() {
-		senhaNormal += 2;
+		if (Fila.getDimensaoFila() > 0 && (Fila.getFila()[Fila.getDimensaoFila() - 1]) % 2 == 0) {
+			senhaNormal = Fila.getFila()[Fila.getDimensaoFila() - 1] + 1;
+		} else {
+			senhaNormal += 2;
+		}
 		return senhaNormal;
 	}
 
@@ -13,4 +17,13 @@ public class Gerador {
 		senhaPreferencial += 2;
 		return senhaPreferencial;
 	}
+
+	public static int getSenhaNormal() {
+		return senhaNormal;
+	}
+
+	public static int getSenhaPreferencial() {
+		return senhaPreferencial;
+	}
+
 }
